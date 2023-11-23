870b258bbcee36912bab81e758c0e26ca7bd6e30
DinaBouazzafen <91288579+DinaBouazzafen@users.noreply.github.com>
2023-11-23 11:32:37 +0100

    Feature(TDD): Test for brand name made

--- images/api/src/helpers/endpointHelpers.js
+++ images/api/src/helpers/endpointHelpers.js
@@ -1,5 +1,13 @@
 function checkBrandName(name){
+if (
+    name == null
+    || name.length <= 1
+    || typeof(name) != "string"
+    || name.length > 20
+) {
     return false
+    }
+    return true
 }
 
 module.exports = {
